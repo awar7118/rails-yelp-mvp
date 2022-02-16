0557ee55e6bf3da98cad4c3dbc69a615421f8b3c
Ahmed Warsama <ahmedwarsama@live.co.uk>
2022-02-16 12:58:38 +0000

    added inclusion validation and presence validation in restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,5 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews
+  validates :category, inclusion: { in:%w(chinese italian japanese french belgian)}
+  validates :name, :address, :category, presence: true
 end
