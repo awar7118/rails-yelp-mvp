4ac3b16135efb4bc7f5b1e52163134b54b0622ee
Ahmed Warsama <ahmedwarsama@live.co.uk>
2022-02-16 15:24:33 +0000

    restaurants route controller and html.erb created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Route for visitor seing the list of all restaurants.
+  # get '/restaurants', to: 'restaurant#index'
+  resources :restaurants, only: [:index, :show,:new,:create] do
+    resources :reviews, only: [:index,:new,:create]
+  end
+
 end
